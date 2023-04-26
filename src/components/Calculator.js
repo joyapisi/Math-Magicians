e730c5ce76540bbb3b51a592cb2c318002a74539
Joy Phoebe <joyapisi@gmail.com>
2023-04-26 23:16:08 +0300

    add events in calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,21 +1,62 @@
-export default function Calculator() {
+import { useState } from 'react';
+import calculate from '../logic/calculate';
+import operate from '../logic/operate';
+
+const obj = {
+  total: null,
+  next: null,
+  operation: null,
+};
+
+function Calculator() {
+  const [output, setOutput] = useState(obj);
+
   const buttonKeys = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
+
+  const clickBtn = (e) => {
+    const btn = e.target.name;
+    return setOutput(calculate(output, btn));
+  };
+
+  const display = (item) => {
+    if ((!item.total) && (!item.next)) {
+      return '0';
+    }
+    if (!item.total) {
+      return item.next;
+    }
+    if (item.next && item.total && item.operation) {
+      return item.next;
+    }
+    return item.total;
+  };
 
   return (
     <section id="calculator">
       <input
         id="terminate"
         type="text"
-        value="0"
+        value={display(output)}
         disabled
       />
       <section id="input">
         <article id="buttons">
           {
-              buttonKeys.map((button) => <button key={button} type="button">{button}</button>)
+              buttonKeys.map((button) => (
+                <button
+                  key={button}
+                  type="button"
+                  name={button}
+                  onClick={clickBtn}
+                >
+                  {button}
+                </button>
+              ))
             }
         </article>
       </section>
     </section>
   );
 }
+
+export default Calculator;
